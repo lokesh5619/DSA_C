83d14ffc8b86fcaa7fb8a7ae797f8b5802999288
Lokesh vishwakarma <lokeshvishwakarma5755@gmail.com>
2026-03-18 11:14:19 +0700

    new

---
+++ Sorting/insertionSort.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+void display(int x[], int n);
+void isort(int x[], int n)
+{
+	int i,j,value;
+	for(i=1; i<n; i++)
+	{
+		value=x[i];
+		for(j=i; j>0; j--)
+		{
+			if(x[j-1] > value)
+			{
+				x[j]=x[j-1];
+			}	
+			else break;
+		}
+	x[j]=value;
+	display(x,n);
+	}
+}
+void display(int x[], int n)
+{
+	int i;
+	for(i=0; i<n; i++)
+	{
+		printf("%d ",x[i]);
+	}
+	printf("\n");
+}
+void main()
+{
+	int n=9;
+	int x[]={56,29,35,42,15,41,75,21,30};
+	display(x,n);
+	isort(x,n);
+}
